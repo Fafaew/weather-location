040838efd25c6235039726cdfadffccca54ab02b
fafaew <rafael.gomest@gmail.com>
2021-10-20 14:53:03 -0300

    General: added search bar and css styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect } from 'react'
 import axios from 'axios'
+import { fetchWeather } from './api/fetchWeather';
 import './App.css'
 import { Card } from 'semantic-ui-react'
 
@@ -7,6 +8,18 @@ import { Card } from 'semantic-ui-react'
 function App() {
   const[location, setLocation] = useState(false);
   const[weather, setWeather] = useState(false);
+
+  const [query, setQuery] = useState('');
+    // const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if(e.key === 'Enter') {
+        const data = await fetchWeather(query);
+
+        setWeather(data);
+        setQuery('');
+    }
+}
 
   let getWeather = async (lat, long) => {
     let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
@@ -58,11 +71,33 @@ function App() {
           <li className="temp">Umidade: {weather['main']['humidity']}%</li>
           </ul>
           </Card.Content>
+          <div className="main-container">
+            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="info">
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+                </div>
+            )}
+        </div>
 
         </Card>
+
+        
       );
 
     }
+    
   }  
 
 
